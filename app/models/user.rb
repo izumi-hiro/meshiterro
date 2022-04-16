ce2e19c69c6ac64f15bae49490f92b909a3b241c
izumi-hiro <transformers2728frost@gmail.com>
2022-04-16 12:15:51 +0000

    [Add] 4-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ def get_profile_image
     file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
     profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
-  profile_image.variant(resize_to_limit: [100, 100])/processed
+  profile_image.variant(resize_to_limit: [width, height]).processed
 end
          
 end
